289cf38539924fa2a282169e3a402bf893deb908
soumya.sitak <soumyasitak.g@gmail.com>
2023-07-08 18:27:34 +0530

    changed

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
+import "../styles/components/contact.css";
+import "../styles/components/footer.css";
 import "../styles/utils.css";
